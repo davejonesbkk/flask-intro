0892190b3e44126ecc2efb12013e4339fa11313b
David Jones <davejonesbkk@gmail.com>
2015-12-08 11:58:31 +0700

    some changes

--- db_create.py
+++ db_create.py
@@ -10,6 +10,7 @@ db.create_all()
 #db.session.add(BlogPost("Flask", "discoverflask.com"))
 #db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
 
+#added a comment for practise with Github
 
 #commit the changes
 db.session.commit()
